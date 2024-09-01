5599624895c202feeb46fd5f8a81a2e011995223
ZainNausheer <zainnausheer007@gmail.com>
2024-09-01 21:28:03 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToe:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tic Tac Toe")
+        self.current_player = "X"
+        self.board = [""] * 9
+        self.buttons = []
+
+        # Create buttons
+        for i in range(9):
+            button = tk.Button(self.root, text="", font=('normal', 20), width=5, height=2, 
+                               command=lambda i=i: self.on_button_click(i))
+            button.grid(row=i // 3, column=i % 3)
+            self.buttons.append(button)
+
+    def on_button_click(self, index):
+        if self.board[index] == "" and not self.check_winner():
+            self.board[index] = self.current_player
+            self.buttons[index].config(text=self.current_player)
+
+            if self.check_winner():
+                messagebox.showinfo("Tic Tac Toe", f"Player {self.current_player} wins!")
+                self.reset_game()
+            elif "" not in self.board:
+                messagebox.showinfo("Tic Tac Toe", "It's a draw!")
+                self.reset_game()
+            else:
+                self.current_player = "O" if self.current_player == "X" else "X"
+
+    def check_winner(self):
+        winning_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical
+            [0, 4, 8], [2, 4, 6]              # Diagonal
+        ]
+
+        for combo in winning_combinations:
+            if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != "":
+                return True
+        return False
+
+    def reset_game(self):
+        self.board = [""] * 9
+        self.current_player = "X"
+        for button in self.buttons:
+            button.config(text="")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
